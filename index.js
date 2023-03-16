93567809408245f16fd8f1806f5a1616da03b5c3
Albert Thuku <athuku02@gmail.com>
2023-03-16 09:05:16 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(){
+    return 2000/100;
+}
+console.log(divide());
+
+const square = num1 => num1 * num1 ;
+console.log(square(5));
+
+const add = (par1, par2) => par1 + par2;
+console.log(add(10,63));
